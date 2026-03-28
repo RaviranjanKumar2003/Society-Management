7b10c190f14474bda4588f72025945f4c1e81780
Raviranjan Gupta <raviranjankumarnd2003@gmail.com>
2026-03-28 19:59:51 +0530

    Comment out Razorpay configuration class

--- Backend/demo/src/main/java/com/example/demo/Configuration/RazorpayConfig.java
+++ Backend/demo/src/main/java/com/example/demo/Configuration/RazorpayConfig.java
@@ -1,21 +1,21 @@
-package com.example.demo.Configuration;
+// package com.example.demo.Configuration;
 
-import com.razorpay.RazorpayClient;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import com.razorpay.RazorpayClient;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class RazorpayConfig {
+// @Configuration
+// public class RazorpayConfig {
 
-    @Value("${razorpay.key}")
-    private String key;
+//     @Value("${razorpay.key}")
+//     private String key;
 
-    @Value("${razorpay.secret}")
-    private String secret;
+//     @Value("${razorpay.secret}")
+//     private String secret;
 
-    @Bean
-    public RazorpayClient razorpayClient() throws Exception {
-        return new RazorpayClient(key, secret);
-    }
-}
+//     @Bean
+//     public RazorpayClient razorpayClient() throws Exception {
+//         return new RazorpayClient(key, secret);
+//     }
+// }
